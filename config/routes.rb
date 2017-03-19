855be6ac81b3908330bdcf72ea4f884e155bedc0
Akshay Naik <akshaynaik93@gmail.com>
2017-03-19 18:06:25 +0530

    devise gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  devise_for :users,controllers: {
+  	sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+
+  }
+
+
+
+
+resources :jobs
+
+devise_scope :user do 
+	root 'users/sessions#new'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
